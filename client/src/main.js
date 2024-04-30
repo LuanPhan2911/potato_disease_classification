64b05986b501411af98ee909d3cd50d29c0545bf
Luan@YokKok <97419663+LuanPhan2911@users.noreply.github.com>
2024-04-30 11:47:28 +0700

    remove client and server, deploy new client

--- client/src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from "vue";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle";
-import App from "./App.vue";
-
-createApp(App).mount("#app");
